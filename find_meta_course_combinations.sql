8606ab1cba89c97ead48cf33bfc963a812b839b7
whanrott <p0084851@brookes.ac.uk>
2017-11-23 14:30:50 +0000

    added where_is_this_content_used.sql

--- find_meta_course_combinations.sql
+++ find_meta_course_combinations.sql
@@ -1,42 +1,36 @@
 
 SELECT --TOP 100
         ct.content_id
-       ,cmt.meta_type_id
-       ,cmt.meta_value
+--       ,ct.private
+--       ,ct.published
        ,mt.meta_name
+--       ,cmt.meta_type_id
+       ,cmt.meta_value
        ,ct.content_title
        ,ct.content_status
        ,fr.FolderPath
---       ,*
 FROM
-	content_meta_tbl   AS cmt,
-	content            AS ct,
-        metadata_type      AS mt,
-        content_folder_tbl AS fr
-WHERE   cmt.content_id      = ct.content_id
-AND     cmt.meta_type_id    = mt.meta_type_id
-and    ct.folder_id         = fr.folder_id
-AND
-		cmt.meta_type_id IN (
-			SELECT meta_type_id
-			FROM   metadata_type
-			WHERE meta_name LIKE '%combination%'
-		)
-AND
-        ct.content_id IN (
-                SELECT content_id
-                FROM content_meta_tbl AS cmt2
-                WHERE cmt2.meta_type_id = 141
-                AND meta_value = 'true'
+content                      AS ct
+LEFT JOIN content_folder_tbl AS fr
+ON ct.folder_id               = fr.folder_id
+LEFT JOIN content_meta_tbl   AS cmt
+ON ct.content_id              = cmt.content_id
+LEFT JOIN metadata_type      AS mt
+ON cmt.meta_type_id           = mt.meta_type_id
+
+WHERE        
+        fr.parent_id IN (
+                SELECT folder_id
+                FROM content_folder_tbl AS fr
+                WHERE fr.FolderPath LIKE ('Studying at Brookes/courses/Undergraduate/0/')
                 )
 AND
-		fr.parent_id IN (
-                        SELECT folder_id
-                        FROM content_folder_tbl AS fr
-                        WHERE fr.FolderPath LIKE ('Studying at Brookes/courses/%/0/')
-                        )
+        ct.content_title NOT LIKE '%DO NOT SET LIVE%'
+AND
+        ct.content_title NOT LIKE '%CLONE%'
+
+AND     cmt.meta_type_id IN    ()
 ORDER BY
-        fr.FolderPath
-       ,ct.content_title
-       ,mt.meta_name
+        ct.content_title
+       ,cmt.meta_type_id
 ;
---
+++ where_is_this_content_used.sql
@@ -0,0 +1,33 @@
+SELECT
+         ct.content_id
+        ,pto.page_id
+        ,fr.FolderPath         AS content_folder
+        ,fr2.FolderPath        AS page_folder
+        ,ct.content_title
+        ,ct2.content_title     AS page_title
+        ,ct.content_status
+        ,ct2.content_status    AS page_status
+        ,ct.private            AS content_private
+        ,ct2.private           AS page_private
+        ,ct.last_edit_fname + ' ' + ct.last_edit_lname   AS content_editor
+        ,convert(varchar,ct.last_edit_date,103)          AS content_edited
+        ,ct2.last_edit_fname + ' ' + ct2.last_edit_lname AS page_editor
+        ,convert(varchar,ct2.last_edit_date,103)         AS page_edited
+
+FROM
+        dbo.content            AS ct,
+        dbo.content_folder_tbl AS fr,
+        dbo.page_to_object     AS pto,
+        dbo.content            AS ct2,
+        dbo.content_folder_tbl AS fr2
+WHERE ct.folder_id         = fr.folder_id
+AND   ct.content_id        = pto.object_id
+AND   pto.page_id          = ct2.content_id
+AND   ct2.folder_id        = fr2.folder_id
+AND   fr.FolderPath     LIKE '%%'
+--AND   ct.content_title like '%%'
+--AND   ct.content_status = ''
+--AND   ct.content_id in ()
+ORDER BY
+        page_folder, page_title
+;

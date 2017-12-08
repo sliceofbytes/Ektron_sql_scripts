6e1407aae59b8c3f3f5b6322bcd021c4b57696ff
whanrott <p0084851@brookes.ac.uk>
2017-12-08 09:36:33 +0000

    updated find_meta_course_combinations.sql

--- find_meta_course_combinations.sql
+++ find_meta_course_combinations.sql
@@ -1,35 +1,36 @@
 
 SELECT --TOP 100
         ct.content_id
---       ,ct.private
---       ,ct.published
-       ,mt.meta_name
---       ,cmt.meta_type_id
-       ,cmt.meta_value
        ,ct.content_title
        ,ct.content_status
-       ,fr.FolderPath
-FROM
-content                      AS ct
+       ,mt.meta_name
+       ,cmt.meta_value
+--       ,ct.private
+--       ,ct.published
+--       ,cmt.meta_type_id
+--       ,fr.FolderPath
+FROM      content            AS ct
 LEFT JOIN content_folder_tbl AS fr
-ON ct.folder_id               = fr.folder_id
+ON        ct.folder_id        = fr.folder_id
 LEFT JOIN content_meta_tbl   AS cmt
-ON ct.content_id              = cmt.content_id
+ON        ct.content_id       = cmt.content_id
 LEFT JOIN metadata_type      AS mt
-ON cmt.meta_type_id           = mt.meta_type_id
+ON        cmt.meta_type_id    = mt.meta_type_id
 
 WHERE        
         fr.parent_id IN (
                 SELECT folder_id
-                FROM content_folder_tbl AS fr
-                WHERE fr.FolderPath LIKE ('Studying at Brookes/courses/Undergraduate/0/')
+                FROM   content_folder_tbl AS fr
+                WHERE  fr.FolderPath      LIKE ('Studying at Brookes/courses/Undergraduate/0/')
                 )
 AND
         ct.content_title NOT LIKE '%DO NOT SET LIVE%'
 AND
         ct.content_title NOT LIKE '%CLONE%'
 
-AND     cmt.meta_type_id IN    ()
+AND     cmt.meta_type_id IN    (139,141,142,143,144,145,146,147,155,157,159,
+                                2147483665,2147483667,2147483668,2147483669,
+                                2147483670,6442450943)
 ORDER BY
         ct.content_title
        ,cmt.meta_type_id
--- where_is_this_content_used.sql
+++ where_is_this_content_used.sql
@@ -13,17 +13,20 @@ SELECT
         ,convert(varchar,ct.last_edit_date,103)          AS content_edited
         ,ct2.last_edit_fname + ' ' + ct2.last_edit_lname AS page_editor
         ,convert(varchar,ct2.last_edit_date,103)         AS page_edited
-
+        ,'https://www.brookes.ac.uk/' + lower(clv.AliasLink) AS url_alias
+--        ,clv.*
 FROM
         dbo.content            AS ct,
         dbo.content_folder_tbl AS fr,
         dbo.page_to_object     AS pto,
         dbo.content            AS ct2,
-        dbo.content_folder_tbl AS fr2
+        dbo.content_folder_tbl AS fr2,
+        dbo.ContentListView    AS clv
 WHERE ct.folder_id         = fr.folder_id
 AND   ct.content_id        = pto.object_id
 AND   pto.page_id          = ct2.content_id
 AND   ct2.folder_id        = fr2.folder_id
+AND   pto.page_id        = clv.content_id
 AND   fr.FolderPath     LIKE '%%'
 --AND   ct.content_title like '%%'
 --AND   ct.content_status = ''
